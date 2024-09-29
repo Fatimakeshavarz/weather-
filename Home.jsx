31cb355d0f03b225342428fbb64d602a65f746a7
Fatimakeshavarz <fatimakeshavarz838@gmail.com>
2024-09-29 20:31:13 +0330

    init commit

---
+++ Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import App from "./App"
+import './App.css'
+
+
+const Home=()=>{
+     
+    return(
+        <div>
+            <App/>
+        </div>
+    )
+}
+
+export default Home
